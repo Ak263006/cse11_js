706906c689623daa7d6e92964b309be735889068
AASHISH KUMAR <aashish.25b01011380>
2026-02-24 09:46:57 +0530

    js

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-console.log("Hello World!");
-document.write("Hello World!");
+// console.log("Hello World!");
+// document.write("Hello World!");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="aashish";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+//function
+// var sayHello=function(){
+//     alert("Hello World!");
+// }
+//array
+// var numberArray=[1,2,3];
+// var animals=newArray("dog","cat","cow");
+ 
+//object
+// 
+// symbol type
+const sym1=symbol(4);
+console.log(sym1);
+const sym2=symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console,log("true");
+}
+else{
+    console.log("false");
+}
