bd5821f98337c2aaf680f689387177edd0c27bcc
AASHISH KUMAR <aashish.25b01011380>
2026-03-10 10:33:47 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,24 @@
+//seclting with id
+// const result= document.getElementById("para");
+// console.log(result);
+//result.innerHTML="Hello World!";//to add the content in the element with id para.
+// result.title="myname";
+// document.write(result);
+
+//selecting with class
+//  const hi= document.getElementsByClassName("p1");
+//  console.log(hi);
+//  hi[0].innerHTML="Hello World!";
+//  hi[1].innerHTML="Welcome to JavaScript";
+//  hi[2].innerHTML="Have a nice day!";
+
+//selecting with tag name
+// const pElements = document.getElementsByTagName("p");
+// pElements[2].innerHTML="This is a paragraph";
+// pElements[1].style.color="blue";
+// pElements[1].innerText="My Paragraph";
+
+//Query selector
+const test=document.querySelectorAll("para");
+test.style.Color="yellow";
+console.log(test);
